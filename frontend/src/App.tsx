a487636cbc5abe69501932fb901e3c387878028b
abereni4u <aberenijopuiyo@gmail.com>
2026-06-05 15:13:54 -0400

    Remove create/delete/edit components and add dashboards to App routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,17 @@
-import { useState } from 'react'
-import {Routes, Route} from 'react-router'
-import './App.css'
-import Login from './components/Login.tsx'
-import Create from './components/Create.tsx'
-import Edit from './components/Edit.tsx'
-import Delete from './components/Delete.tsx'
-
+import { Routes, Route } from "react-router";
+import "./App.css";
+import Login from "./components/Login";
+import InstructorDashboard from "./components/InstructorDashboard";
+import StudentDashboard from "./components/StudentDashboard";
 
 function App() {
   return (
-    <>
-
-      <Routes>
-          <Route path="" element={<Login/>}/>
-          <Route path="/edit:id" element={<Edit/>}/>
-          <Route path="/create" element={<Create/>}/>
-          <Route path="/delete:id" element={<Delete/>}/>
-      </Routes>
-
-   </>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/instructor" element={<InstructorDashboard />} />
+      <Route path="/student" element={<StudentDashboard />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
